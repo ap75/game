20959a6bdaa9c8071bc5c59c32a018b93d8183a4
ap75 <p.aleksieiev@rl.kyiv.ua>
2025-04-17 09:28:18 +0300

    Add data model

--- bulls_and_cows/models.py
+++ bulls_and_cows/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Game(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='games')
+    number = models.PositiveIntegerField() # validators 1000..9999
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Game #{self.pk} for {self.user.username}"
+
+    @property
+    def is_finished(self):
+        return self.tries.filter(bull=4).exists()
+
+    @property
+    def tries_count(self):
+        return self.tries.count()
+
+
+class Try(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='tries')
+    guess = models.PositiveIntegerField() # validators 1000..9999
+    bulls = models.PositiveIntegerField()
+    cows = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Try {self.guess}"
